216d1842b815158a8ed11fc93a05e3f12a8cef06
Gaurav Kumar Yadav <gaurav.yadav1@olx.com>
2022-12-23 18:09:22 +0530

    seo changes for js,html & css formatter

--- src/environments/component-config.ts
+++ src/environments/component-config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig } from 'src/app/@types/config';
 
 export const home: ApplicationConfig = {
   navigationUrl: '',
-  pageTitle: 'Free Web Tools',
+  pageTitle: 'Free Web Tools - webtoolseasy.com',
   metaTags: [
     {
       name: 'description',
@@ -12,7 +12,7 @@ export const home: ApplicationConfig = {
     {
       name: 'keywords',
       content:
-        'Free Web Tools, JWT Decode, UUID Generator, JSON Formatter, Compress JPEG, Compress PNG, JS Formatter, HTML Formatter, CSS Formatter',
+        'free web tools, jwt decode, uuid generator, json formatter, compress jpeg, compress png, js formatter, html formatter, css formatter',
     },
     {
       name: 'author',
@@ -71,17 +71,17 @@ export const home: ApplicationConfig = {
 
 export const tools: ApplicationConfig = {
   navigationUrl: '/tools',
-  pageTitle: 'Web Tools Home',
+  pageTitle: 'Free Web Tools - Tools Home',
   metaTags: [
     {
       name: 'description',
       content:
-        'Web Tools Home | UUID Generator | Compress JPEG, Compress PNG | JWT Decode | JS Formatter | JSON Formatter | CSS Formatter | HTML Formatter',
+        'UUID Generator | Compress JPEG, Compress PNG | JWT Decode | JS Formatter | JSON Formatter | CSS Formatter | HTML Formatter',
     },
     {
       name: 'keywords',
       content:
-        'Free Web Tools, JWT Decode, UUID Generator, JSON Formatter, Compress JPEG, Compress PNG, JS Formatter, HTML Formatter, CSS Formatter',
+        'free web tools, jwt decode, uuid generator, json formatter, compress jpeg, compress png, js formatter, html formatter, css formatter',
     },
     {
       name: 'author',
@@ -146,7 +146,7 @@ export const uuid: ApplicationConfig = {
     {
       name: 'keywords',
       content:
-        'UUID Generator, Generate UUID, Generate UUID V4, Generate UUID V1, Online UUID Generator, UUID V4, UUID V1',
+        'uuid generator, generate uuid, generate uuid v4, generate uuid v1, online uuid generator, uuid v4, uuid v1',
     },
     {
       name: 'author',
@@ -189,7 +189,7 @@ export const jwt: ApplicationConfig = {
     {
       name: 'keywords',
       content:
-        'JWT Decode, JWT Decoder, JWT Token Decode, JWT Decode HS256, JSON Web Token',
+        'jwt decode, jwt decoder, jwt token decode, jwt decode hs256, json web token',
     },
     {
       name: 'author',
@@ -203,77 +203,6 @@ export const jwt: ApplicationConfig = {
     'decode jwt',
     'jwt decode online',
     'jwt hs256',
-  ],
-  icons: [
-    {
-      iconName: 'app-icon',
-      iconRelativeUrl: 'app-icon.svg',
-    },
-    {
-      iconName: 'linkedin-icon',
-      iconRelativeUrl: 'linkedin.svg',
-    },
-    {
-      iconName: 'facebook-icon',
-      iconRelativeUrl: 'facebook.svg',
-    },
-  ],
-};
-
-export const jsonformatter: ApplicationConfig = {
-  navigationUrl: '/tools/json-formatter',
-  pageTitle: 'JSON Formatter | Web Tools Easy',
-  metaTags: [
-    {
-      name: 'description',
-      content:
-        'JSON Formatter | Format JSON Online | Beautify JSON String Online',
-    },
-    {
-      name: 'keywords',
-      content: 'JSON Formatter, Format JSON Online, Beautify JSON Online, JSON',
-    },
-    {
-      name: 'author',
-      content: 'Gaurav Kumar Yadav',
-    },
-    { name: 'robots', content: 'index, follow' },
-  ],
-  tags: ['json formatter', 'beautify json', 'format json online'],
-  icons: [
-    {
-      iconName: 'app-icon',
-      iconRelativeUrl: 'app-icon.svg',
-    },
-  ],
-};
-
-export const jsformatter: ApplicationConfig = {
-  navigationUrl: '/tools/js-formatter',
-  pageTitle: 'JS Formatter | JS Beautifier',
-  metaTags: [
-    {
-      name: 'description',
-      content:
-        'JS Formatter | JS Beautifier | Javascript Formatter | String Format Javascript | Javascript Prettify',
-    },
-    {
-      name: 'keywords',
-      content:
-        'JS Formatter, JS Beautifier, Javascript Formatter, String Format Javascript, Javascript Prettify',
-    },
-    {
-      name: 'author',
-      content: 'Gaurav Kumar Yadav',
-    },
-    { name: 'robots', content: 'index, follow' },
-  ],
-  tags: [
-    'js formatter',
-    'js beautifier',
-    'string format javascript',
-    'javascript prettify',
-    'javascript formatter',
   ],
   icons: [
     {
@@ -351,26 +280,104 @@ export const imagecompress: ApplicationConfig = {
   ],
 };
 
-export const soon: ApplicationConfig = {
-  navigationUrl: '',
-  pageTitle: '',
-  metaTags: [],
-  tags: [],
-  icons: [],
-};
-
-export const cssformatter = {
-  navigationUrl: '/tools/css-formatter',
-  pageTitle: 'CSS Formatter | Web Tools Easy',
+export const jsonformatter: ApplicationConfig = {
+  navigationUrl: '/tools/json-formatter',
+  pageTitle: 'JSON Formatter | JSON Beautifier',
   metaTags: [
     {
       name: 'description',
-      content: 'CSS Formatter | Format CSS Online | Beautify CSS Online',
+      content:
+        'JSON Formatter | JSON Beautifier | Best JSON Formatter | JSON Formatter Online',
     },
     {
       name: 'keywords',
       content:
-        'CSS Formatter, Format CSS Online, Format CSS, Beautify CSS online',
+        'json formatter, json beautifier, best json formatter, json formatter online, json',
+    },
+    {
+      name: 'author',
+      content: 'Gaurav Kumar Yadav',
+    },
+    { name: 'robots', content: 'index, follow' },
+  ],
+  tags: [
+    'json formatter',
+    'json beautifier',
+    'best json formatter',
+    'json formatter online',
+  ],
+  icons: [
+    {
+      iconName: 'app-icon',
+      iconRelativeUrl: 'app-icon.svg',
+    },
+    {
+      iconName: 'linkedin-icon',
+      iconRelativeUrl: 'linkedin.svg',
+    },
+    {
+      iconName: 'facebook-icon',
+      iconRelativeUrl: 'facebook.svg',
+    },
+  ],
+};
+
+export const jsformatter: ApplicationConfig = {
+  navigationUrl: '/tools/js-formatter',
+  pageTitle: 'JS Formatter | JS Beautifier',
+  metaTags: [
+    {
+      name: 'description',
+      content:
+        'JS Formatter | JS Beautifier | Javascript Formatter | String Format Javascript | Javascript Prettify',
+    },
+    {
+      name: 'keywords',
+      content:
+        'JS Formatter, JS Beautifier, Javascript Formatter, String Format Javascript, Javascript Prettify',
+    },
+    {
+      name: 'author',
+      content: 'Gaurav Kumar Yadav',
+    },
+    { name: 'robots', content: 'index, follow' },
+  ],
+  tags: [
+    'js formatter',
+    'js beautifier',
+    'string format javascript',
+    'javascript prettify',
+    'javascript formatter',
+  ],
+  icons: [
+    {
+      iconName: 'app-icon',
+      iconRelativeUrl: 'app-icon.svg',
+    },
+    {
+      iconName: 'linkedin-icon',
+      iconRelativeUrl: 'linkedin.svg',
+    },
+    {
+      iconName: 'facebook-icon',
+      iconRelativeUrl: 'facebook.svg',
+    },
+  ],
+};
+
+export const cssformatter = {
+  navigationUrl: '/tools/css-formatter',
+  pageTitle: 'CSS Formatter | CSS Beautifier',
+  metaTags: [
+    {
+      name: 'description',
+      content:
+        'CSS Formatter | CSS Beautifier | CSS Prettify | CSS Formatter Online | CSS Beautifier Online',
+    },
+    {
+      name: 'keywords',
+      content:
+        'css formatter | css beautifier | css prettify | css formatter online | css beautifier online',
     },
     {
       name: 'author',
@@ -380,31 +387,40 @@ export const cssformatter = {
   ],
   tags: [
     'css formatter',
-    'css beautify',
-    'format css online',
-    'beautify css online',
+    'css beautifier',
+    'css prettify',
+    'css formatter online',
+    'css beautifier online',
   ],
   icons: [
     {
       iconName: 'app-icon',
       iconRelativeUrl: 'app-icon.svg',
     },
+    {
+      iconName: 'linkedin-icon',
+      iconRelativeUrl: 'linkedin.svg',
+    },
+    {
+      iconName: 'facebook-icon',
+      iconRelativeUrl: 'facebook.svg',
+    },
   ],
 };
 
 export const htmlformatter = {
   navigationUrl: '/tools/html-formatter',
-  pageTitle: 'HTML Formatter | Web Tools Easy',
+  pageTitle: 'HTML Formatter | HTML Beautify',
   metaTags: [
     {
       name: 'description',
       content:
-        'HTML Formatter | Format HTML Online | Beautify HTML Code Online',
+        'HTML Formatter | HTML Beautify | HTML Formatter Online | HTML Code Formatter',
     },
     {
       name: 'keywords',
       content:
-        'HTML Formatter, Format HTML Online, Format HTML, Beautify HTML Online',
+        'html formatter, html beautify, html formatter online, html code formatter',
     },
     {
       name: 'author',
@@ -415,13 +431,29 @@ export const htmlformatter = {
   tags: [
     'html formatter',
     'html beautify',
-    'format html online',
-    'beautify html online',
+    'fhtml formatter online',
+    'html code formatter',
   ],
   icons: [
     {
       iconName: 'app-icon',
       iconRelativeUrl: 'app-icon.svg',
     },
+    {
+      iconName: 'linkedin-icon',
+      iconRelativeUrl: 'linkedin.svg',
+    },
+    {
+      iconName: 'facebook-icon',
+      iconRelativeUrl: 'facebook.svg',
+    },
   ],
+};
+
+export const soon: ApplicationConfig = {
+  navigationUrl: '',
+  pageTitle: '',
+  metaTags: [],
+  tags: [],
+  icons: [],
 };
