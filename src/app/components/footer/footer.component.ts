a77895faf5fd441a0469b5dcdfef965dbd3e678e
Gaurav Kumar Yadav <gaurav.yadav1@olx.com>
2023-02-12 22:16:39 +0530

    added footer in all components + copyright

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  openLink(url: string) {
+    window.open(url, '_blank')?.focus();
+  }
+}
