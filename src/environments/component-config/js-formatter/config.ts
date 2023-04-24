49c3dd40c0335b914fb890245b930bf29257a75c
Gaurav Kumar Yadav <gaurav.yadav1@olx.com>
2023-04-24 17:01:38 +0530

    text compare + js formatter seo changes

--- src/environments/component-config/js-formatter/config.ts
+++ src/environments/component-config/js-formatter/config.ts
@@ -2,15 +2,15 @@ import { ApplicationConfig } from 'src/app/@types/config';
 import { DescriptionBlock } from 'src/app/@types/description';
 
 export const componentConfig: ApplicationConfig = {
-  mainHeading: 'JS Formatter',
-  subHeading: 'Online JS Beautifier',
+  mainHeading: 'Javascript Formatter',
+  subHeading: 'Javascript Beautifier',
   navigationUrl: '/tools/js-formatter',
-  pageTitle: 'JS Formatter | JS Beautifier',
+  pageTitle: 'Best Online Javascript Beautifier / Formatter',
   metaTags: [
     {
       name: 'description',
       content:
-        'JS Formatter | JS Beautifier | Javascript Formatter | String Format Javascript | Javascript Prettify',
+        'Javascript Formatter / Beautifier tool lets you beautify / format your JavaScript code for free',
     },
     {
       name: 'author',
@@ -24,6 +24,7 @@ export const componentConfig: ApplicationConfig = {
     'string format javascript',
     'javascript prettify',
     'javascript formatter',
+    'javascript beautifier',
   ],
   icons: [
     {
@@ -75,27 +76,51 @@ export const componentConfig: ApplicationConfig = {
 
 export const descriptionData: DescriptionBlock[] = [
   {
-    heading: 'How to use JS Formatter/Beautifier',
+    heading: 'How to use Javascript Formatter / Beautifier?',
     listData: [
-      `Paste unformatted/minified javascript in unformatted js block and code will be formatted automatically.`,
-      `Javascript formatting is being done locally in browser only.`,
+      `Paste unformatted / minified Javascript in unformatted Javascript block and code it be formatted / beautified automatically.`,
     ],
   },
   {
-    heading: 'Key features',
-    listData: [
-      `Easy to use: Our user-friendly interface makes it simple for anyone to format their JavaScript code with just pasting code here.`,
-      `Real-time formatting: Our tool formats your code in real-time, so you can see the changes as you make them.`,
-      `No need to download any software. Simply paste your code into our platform and get a clean, organized, and optimized result.`,
-    ],
-  },
-  {
-    heading: 'Why Choose Our Online JavaScript Formatter Tool?',
+    heading: 'Why choose our Online JavaScript Formatter / Beautifier Tool?',
     listData: [
       `Improves readability: Clean and organized code is easier to read and understand, making it easier for you or your team to work with.`,
       `Saves time: Automated formatting and optimization saves you time and effort compared to manual formatting.`,
       `Supports collaboration: Clean, organized code makes it easier for multiple people to work on the same project, improving collaboration and reducing the risk of errors.`,
       `Security: Your data is 100% secure on our platform as whole processing is being done in host browser only.`,
+    ],
+  },
+  {
+    heading: `What is Javascript?`,
+    blockData: [
+      `JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles.`,
+      `JavaScript's dynamic capabilities include runtime object construction, variable parameter lists, function variables, dynamic script creation (via eval), object introspection (via for...in and Object utilities), and source-code recovery (JavaScript functions store their source text and can be retrieved through toString()).`,
+    ],
+  },
+  {
+    heading: 'Key features',
+    listData: [
+      `Easy to use: Our user-friendly interface makes it simple for anyone to format their JavaScript code with just pasting Javascript here.`,
+      `Real-time formatting: Our tool formats your Javascript in real-time, so you can see the changes as you make them.`,
+      `No need to download any software. Simply paste your code into our platform and get a clean, organized, and optimized result.`,
+      `Formatted / Beautified Javascript can be saved or copied easily.`,
+    ],
+  },
+  {
+    heading: 'References',
+    links: [
+      {
+        displayText: 'Read more about Javascript at Wikipedia',
+        url: 'https://en.wikipedia.org/wiki/JavaScript',
+      },
+      {
+        displayText: 'Learn Javascript at w3schools.com',
+        url: 'https://www.w3schools.com/js/DEFAULT.asp',
+      },
+      {
+        displayText: 'Read about Javascript at Mozilla Developer Docs',
+        url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+      },
     ],
   },
 ];
--- src/environments/component-config/text-compare/config.ts
+++ src/environments/component-config/text-compare/config.ts
@@ -5,12 +5,13 @@ export const componentConfig: ApplicationConfig = {
   mainHeading: 'Text File Difference',
   subHeading: 'Compare Text Files Online',
   navigationUrl: '/tools/text-compare',
-  pageTitle: 'Text File Compare | Text File Difference',
+  pageTitle:
+    'Compare text online to find the difference between two text files',
   metaTags: [
     {
       name: 'description',
       content:
-        'Text Difference Checker | Diff checker online | Text Compare | File Compare | Compare Files Online | Diff Tool',
+        'Text File Difference tool will compare text to find the difference between two text files. Just paste your files and Find Difference!',
     },
     {
       name: 'author',
@@ -45,31 +46,32 @@ export const descriptionData: DescriptionBlock[] = [
   {
     heading: 'How to compare text',
     listData: [
-      `Paste original text in text 1 area or upload a file 1 to compare`,
-      `Paste target text in text 2 area or upload a file 2 to compare`,
-      `Difference between both the texts will be evaluated automatically and will be reflected in difference area`,
-      `Default text comparion type is charcter by charcter, which can be changed via comparison type dropdown to any of the available comparison type like word by word or line by line`,
-      `To ignore case while comparing, use the corresponding checkbox`,
-      `To ignore leading & trailing whitespace while comparing, use the corresponding checkbox`,
-      `For swapping text 1 & text 2 with each other use the switch texts button`,
-      `After making any of the above change difference between both the texts will be evaluated automatically`,
+      `Paste original text in text 1 area or upload a file 1 to compare.`,
+      `Paste target text in text 2 area or upload a file 2 to compare.`,
+      `Difference between both the texts will be evaluated automatically and will be reflected in difference area.`,
+      `Default text comparion type is charcter by charcter, which can be changed via comparison type dropdown to any of the available comparison type like word by word or line by line.`,
+      `To ignore case while comparing, use the corresponding checkbox.`,
+      `To ignore leading & trailing whitespace while comparing, use the corresponding checkbox.`,
+      `For swapping text 1 & text 2 with each other use the switch texts button.`,
+      `After making any of the above change difference between both the texts will be evaluated automatically.`,
+    ],
+  },
+  {
+    heading: 'What is the need of a text file comparison / difference tool?',
+    listData: [
+      `An online text file comparison / difference tool is an essential tool for writers, students, researchers, and anyone who needs to compare texts quickly and easily. With this tool, you can compare different versions of a text, identify similarities and differences between texts, and highlight changes between texts.`,
+      `This tool uses advanced algorithms to accurately compare texts, making it easy for you to identify differences, changes, and similarities. Whether you're working on an academic project, a research paper, or a business report, an online text comparison tool can help you get the job done faster and more accurately.`,
     ],
   },
   {
     heading: 'Key features',
     listData: [
-      `Text file comparsion is 100% free`,
-      `Text file comparison is 100% secure as whole processing is being done at client side only (in host browser)`,
-      `Unlimited text file comparison`,
-      `Track down plagiarism`,
+      `Text file comparsion / difference tool is 100% free.`,
+      `Text file comparison / difference is 100% secure as whole processing is being done at client side only (in host browser).`,
+      `Unlimited text file comparison.`,
+      `Track down plagiarism.`,
       `Compare text files character by character, word by word or line by line`,
-    ],
-  },
-  {
-    heading: 'What is the need of a text file comparison tool?',
-    blockData: [
-      `An online text file comparison tool is an essential tool for writers, students, researchers, and anyone who needs to compare texts quickly and easily. With this tool, you can compare different versions of a text, identify similarities and differences between texts, and highlight changes between texts.`,
-      `This tool uses advanced algorithms to accurately compare texts, making it easy for you to identify differences, changes, and similarities. Whether you're working on an academic project, a research paper, or a business report, an online text comparison tool can help you get the job done faster and more accurately.`,
+      `Optionally, you can ignore whitepsace.`,
     ],
   },
 ];
