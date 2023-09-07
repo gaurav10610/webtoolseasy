3df8d9c12cfd8c0e7838d3903ece4476b6a49e3e
Gaurav Kumar Yadav <gaurav.yadav1@olx.com>
2023-09-07 13:42:21 +0530

    created image cropper app

---
+++ src/app/image-cropper/image-cropper.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ImageCropperRoutingModule } from './image-cropper-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ImageCropperRoutingModule
+  ]
+})
+export class ImageCropperModule { }
