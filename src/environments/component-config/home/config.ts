ecaa5ccbfe8690ce9a08caf899a5facf31270a1b
Gaurav Kumar Yadav <gaurav.yadav1@olx.com>
2023-09-20 12:18:53 +0530

    title updated

--- src/environments/component-config/home/config.ts
+++ src/environments/component-config/home/config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig } from 'src/app/@types/config';
 import { environment } from 'src/environments/environment';
 
 const navigationUrl = '';
-const pageTitle = 'Free Online Web Tools - WebToolsEasy';
+const pageTitle = 'Free Online Web Tools';
 const pageDescription =
   'Discover a range of free online web tools that can simplify your work and boost productivity. Try them out today!';
 const imageUrl = `${environment.screenshotsBaseUrl}/home-page.png`;
