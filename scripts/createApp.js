f51585f08c2a1a4941d7b55939573abd07dc3a5e
Gaurav Kumar Yadav <gaurav.yadav1@olx.com>
2023-09-25 01:08:11 +0530

    added scripts to add new app in apps.json

--- scripts/createApp.js
+++ scripts/createApp.js
@@ -2,7 +2,11 @@ var fs = require('fs');
 var { XMLParser, XMLBuilder } = require('fast-xml-parser');
 var xmlFormat = require('xml-formatter');
 
-function addUrlInSitemap(newAppUrl) {
+/**
+ * add the new app's url in 'src/sitemap.xml'
+ * @param {*} newAppName 
+ */
+function addUrlInSitemap(newAppName) {
   var siteMapData = fs.readFileSync('./src/sitemap.xml', {
     encoding: 'UTF-8',
   });
@@ -16,7 +20,7 @@ function addUrlInSitemap(newAppUrl) {
 
   // add new tool url
   jObj.urlset.url.push({
-    loc: `https://webtoolseasy.com/tools/${newAppUrl}`,
+    loc: `https://webtoolseasy.com/tools/${newAppName}`,
     lastmod: new Date().toISOString().split('T')[0],
   });
 
@@ -32,7 +36,10 @@ function addUrlInSitemap(newAppUrl) {
   });
 }
 
-function createNewAppConfig() {
+/**
+ * create app's component config in 'src/environments/component-config/new-app'
+ */
+function createNewAppComponentConfig() {
   console.log('creating app config for new app: ' + process.env.APP_NAME);
   var configPath =
     './src/environments/component-config/' + process.env.APP_NAME;
@@ -44,6 +51,31 @@ function createNewAppConfig() {
   );
 }
 
-createNewAppConfig();
+/**
+ * register new app in 'src/environments/apps.json'
+ * @param {*} newAppName 
+ */
+function registerNewApp(newAppName) {
+  const appsConfigJson = JSON.parse(fs.readFileSync('./src/environments/apps.json', {
+    encoding: 'utf8'
+  }));
+  
+  const applicationId = newAppName.replaceAll('-','')
+  appsConfigJson[applicationId] = {
+    applicationId,
+    displayText: newAppName,
+    iconName: `${newAppName}-icon`,
+    navigateUrl: `/tools/${newAppName}`,
+    iconRelativeUrl: ''
+  }
+
+  fs.writeFileSync('./src/environments/apps.json', JSON.stringify(appsConfigJson), {
+    encoding: 'utf8'
+  });
+}
+
+createNewAppComponentConfig();
 
 addUrlInSitemap(process.env.APP_NAME);
+
+registerNewApp(process.env.APP_NAME);
