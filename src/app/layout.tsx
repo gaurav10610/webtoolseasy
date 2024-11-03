03367d2c28e43b99a5aa44d9c29b8d2e150dc7f5
Gaurav Kumar Yadav <gaurav@Gauravs-MacBook-Pro-2.local>
2024-11-03 13:22:39 +0530

    updated metadata for home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,11 +13,40 @@ import ApplicationIcon from "@/data/icons/app-icon.svg";
 import { CustomSvgIcon } from "@/components/lib/icons";
 import HomeIcon from "@/data/icons/home.svg";
 
+const pageTitle = "Online Web Tools: Browse Free Tools to Boost Productivity";
+const pageDescription =
+  "WebToolsEasy features multiple free online web tools. Find the perfect tool for your needs, whether you're looking for a way to edit photos, or record a screen.";
+
 export const metadata: Metadata = {
-  title: "Online Web Tools: Browse Free Tools to Boost Productivity",
-  description:
-    "WebToolsEasy features multiple free online web tools. Find the perfect tool for your needs, whether you're looking for a way to edit photos, or record a screen.",
+  title: pageTitle,
+  description: pageDescription,
   icons: "/favicon.png",
+  authors: {
+    name: "Gaurav Kumar Yadav",
+  },
+  robots: "index, follow",
+  openGraph: {
+    title: pageTitle,
+    type: "website",
+    url: `${process.env.HOSTNAME}`,
+    images: [
+      {
+        url: `${process.env.SCREENSHOTS_BASE_URL}/tools-directory.png`,
+        secureUrl: `${process.env.SCREENSHOTS_BASE_URL}/tools-directory.png`,
+        alt: pageTitle,
+      },
+    ],
+    description: pageDescription,
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@webtoolseasy",
+    title: pageTitle,
+    description: pageDescription,
+    images: [`${process.env.SCREENSHOTS_BASE_URL}/tools-directory.png`],
+  },
+  keywords:
+    "web tools, online tools, web development, web design, HTML tools, CSS tools, JavaScript tools, SEO tools, image compression, code formatter, JSON formatter, URL encoder, URL decoder, base64 encoder, base64 decoder, text editor, color picker, regex tester, lorem ipsum generator, password generator, hash generator, QR code generator, web utilities",
 };
 
 export default function RootLayout({
