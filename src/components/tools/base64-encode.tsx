b0da7d4b2822204bbf0e8639f6de228c8769adc5
Gaurav Kumar Yadav <gaurav@Gauravs-MacBook-Pro-2.local>
2024-12-25 14:32:45 +0530

    base 64 application

---
+++ src/components/tools/base64-encode.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import InsertDriveFileRoundedIcon from "@mui/icons-material/InsertDriveFileRounded";
+import { useState } from "react";
+import { Typography } from "@mui/material";
+import { ButtonWithHandler } from "../lib/buttons";
+import { isEmpty } from "lodash-es";
+import { copyToClipboard } from "@/util/commonUtils";
+import { SnackBarWithPosition } from "../lib/snackBar";
+
+export default function Base64Encode() {
+  const [base64Data, setBase64Data] = useState<string>("");
+  const [isSnackBarOpen, setIsSnackBarOpen] = useState(false);
+  const [snackBarMessage, setSnackBarMessage] = useState("Base64 Data Copied!");
+
+  const handleSnackBarClose = () => {
+    setIsSnackBarOpen(false);
+  };
+
+  const handleSelectFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) {
+      return;
+    }
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      const base64Data = reader.result;
+      setBase64Data(base64Data as string);
+    };
+  };
+
+  const openFileDialog = () => {
+    const input = document.getElementById("file") as HTMLInputElement;
+    input.type = "file";
+    input.click();
+  };
+
+  const copyBase64Data = () => {
+    copyToClipboard(base64Data);
+    setIsSnackBarOpen(true);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full gap-2">
+      <SnackBarWithPosition
+        message={snackBarMessage}
+        open={isSnackBarOpen}
+        autoHideDuration={2000}
+        handleClose={handleSnackBarClose}
+      />
+      <div className="flex flex-col items-center justify-center w-full border-solid border-2 border-lightgrey rounded-sm p-4">
+        <InsertDriveFileRoundedIcon fontSize="large" />
+        <Typography variant="body2">or</Typography>
+        <input
+          id="file"
+          type="file"
+          className="hidden"
+          onChange={handleSelectFile}
+        />
+        <ButtonWithHandler
+          onClick={openFileDialog}
+          buttonText="Browse File"
+          size="small"
+          variant="outlined"
+        />
+      </div>
+
+      <div className="flex flex-row w-full justify-end">
+        <ButtonWithHandler
+          buttonText="Copy Base64 Data"
+          onClick={copyBase64Data}
+          size="small"
+        />
+      </div>
+
+      <div className="flex flex-col w-full">
+        <Typography variant="h3" sx={{ fontSize: "inherit" }}>
+          Base64 Encoded Data
+        </Typography>
+        <div className="w-full border-solid border-2 border-lightgrey rounded-sm p-4">
+          <Typography variant="body2" sx={{ overflowWrap: "anywhere" }}>
+            {base64Data}
+          </Typography>
+        </div>
+      </div>
+    </div>
+  );
+}
