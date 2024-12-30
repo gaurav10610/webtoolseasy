1bd15217b8e69e584579ae00e15ffaa5b9c163ef
Gaurav Kumar Yadav <gaurav@Gauravs-MacBook-Pro-2.local>
2024-12-30 22:49:50 +0530

    image cropper

---
+++ src/components/fileComponents.tsx
@@ -0,0 +1,40 @@
+import InsertDriveFileRoundedIcon from "@mui/icons-material/InsertDriveFileRounded";
+import { Typography } from "@mui/material";
+import { ButtonWithHandler } from "./lib/buttons";
+import Image from "next/image";
+
+export const NoFilesState = ({
+  openFileDialog,
+}: {
+  openFileDialog: () => void;
+}) => {
+  return (
+    <div className="flex flex-col items-center justify-center w-full border-solid border-2 border-gray-300 rounded-sm p-4">
+      <InsertDriveFileRoundedIcon fontSize="large" />
+      <Typography variant="body2">or</Typography>
+      <ButtonWithHandler
+        onClick={openFileDialog}
+        buttonText="Browse File"
+        size="small"
+        variant="outlined"
+      />
+    </div>
+  );
+};
+
+export const ImagesPreview = ({ fileList }: { fileList: File[] }) => {
+  return (
+    <div className="flex flex-row gap-4 overflow-y-auto w-full h-36 md:h-50">
+      {fileList.map((file, index) => (
+        <Image
+          key={index}
+          src={URL.createObjectURL(file)}
+          alt={file.name}
+          className="h-full object-cover rounded-md border-2 border-gray-300"
+          width={128}
+          height={128}
+        />
+      ))}
+    </div>
+  );
+};
---
+++ src/components/tools/crop-image.tsx
@@ -0,0 +1,214 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { isEmpty, isNil, map, toUpper } from "lodash-es";
+import { useState } from "react";
+import { ImagesPreview, NoFilesState } from "../fileComponents";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Typography,
+} from "@mui/material";
+import ReactCrop, { Crop } from "react-image-crop";
+import "react-image-crop/dist/ReactCrop.css";
+import { ButtonWithHandler } from "../lib/buttons";
+import DownloadIcon from "@mui/icons-material/Download";
+
+export default function CropImage() {
+  const [fileList, setFileList] = useState<File[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [crop, setCrop] = useState<Crop>({
+    unit: "px", // Can be 'px' or '%'
+    x: 25,
+    y: 25,
+    width: 50,
+    height: 50,
+  });
+  const formatList = ["png", "jpeg", "webp", "bmp", "ico"];
+  const [imageFormat, setImageFormat] = useState<string>(formatList[0]);
+
+  const onFilesSelection = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) {
+      return;
+    }
+    const files = event.target.files;
+    setFileList([...Array.from(files)]);
+    if (isNil(selectedFile)) {
+      setSelectedFile(files[0]);
+    }
+  };
+
+  const openFileDialog = () => {
+    const input = document.getElementById("file") as HTMLInputElement;
+    input.type = "file";
+    input.click();
+  };
+
+  const croppedImage = ({
+    crop,
+    selectedFile,
+    callback,
+  }: Readonly<{
+    crop: Crop;
+    selectedFile: File;
+    imageFormat: string;
+    callback: (blob: Blob | null) => void;
+  }>) => {
+    console.log("Cropping image", { crop });
+    const canvas = document.createElement("canvas");
+    const image = new Image();
+    image.src = URL.createObjectURL(selectedFile);
+
+    image.onload = () => {
+      canvas.width = crop.width;
+      canvas.height = crop.height;
+      const ctx = canvas.getContext("2d");
+
+      if (ctx) {
+        ctx.drawImage(
+          image,
+          crop.x,
+          crop.y,
+          crop.width,
+          crop.height, // Source rectangle
+          0,
+          0,
+          crop.width,
+          crop.height // Destination rectangle
+        );
+
+        canvas.toBlob(callback);
+      }
+    };
+  };
+
+  const formatOptions = map(formatList, (item) => {
+    return (
+      <MenuItem key={item} value={item}>
+        {toUpper(item)}
+      </MenuItem>
+    );
+  });
+
+  const handleImageFormatChange = (event: SelectChangeEvent<string>) => {
+    setImageFormat(event.target.value);
+  };
+
+  const downloadCallback = (blob: Blob | null) => {
+    if (!blob) {
+      console.error("Failed to crop image");
+      return;
+    }
+
+    const element = document.createElement("a");
+    element.href = URL.createObjectURL(blob);
+    element.download = `cropped-image.${imageFormat}`;
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
+
+  const downloadImage = ({
+    crop,
+    selectedFile,
+    imageFormat,
+    callback,
+  }: Readonly<{
+    crop: Crop;
+    selectedFile: File;
+    imageFormat: string;
+    callback: (blob: Blob | null) => void;
+  }>) => {
+    if (!selectedFile) {
+      console.error("No file selected");
+      return;
+    }
+
+    croppedImage({
+      crop,
+      selectedFile,
+      imageFormat,
+      callback,
+    });
+  };
+
+  const DownloadImageButtons = () => {
+    return (
+      <div className="flex flex-row w-full gap-2 justify-end">
+        <div className="w-[8rem]">
+          <FormControl variant="outlined" size="small" fullWidth>
+            <InputLabel>Output Format</InputLabel>
+            <Select
+              value={imageFormat}
+              label="Output Format"
+              onChange={handleImageFormatChange}
+            >
+              {formatOptions}
+            </Select>
+          </FormControl>
+        </div>
+
+        <ButtonWithHandler
+          buttonText="Download Image"
+          startIcon={<DownloadIcon />}
+          onClick={() => {
+            downloadImage({
+              crop,
+              selectedFile: selectedFile as File,
+              imageFormat,
+              callback: downloadCallback,
+            });
+          }}
+        />
+      </div>
+    );
+  };
+
+  return (
+    <div className="flex flex-col w-full gap-3">
+      <input
+        id="file"
+        type="file"
+        className="hidden"
+        onChange={onFilesSelection}
+        multiple
+        accept=".jpg,.jpeg,.png,.webp,.bmp"
+      />
+      {isEmpty(fileList) && <NoFilesState openFileDialog={openFileDialog} />}
+      {!isEmpty(fileList) && (
+        <Typography variant="h5" color="primary">
+          Selected Images
+        </Typography>
+      )}
+      {!isEmpty(fileList) && <ImagesPreview fileList={fileList} />}
+      {!isNil(selectedFile) && (
+        <Typography variant="h5" color="primary">
+          Crop & Preview
+        </Typography>
+      )}
+
+      {!isNil(selectedFile) && <DownloadImageButtons />}
+      {!isNil(selectedFile) && (
+        <ReactCrop
+          crop={crop}
+          onChange={(c) => {
+            setCrop({
+              ...crop,
+              ...c,
+            });
+          }}
+          className="w-full max-h-fit border-solid border-2 border-gray-300 p-4 rounded-md"
+        >
+          <img
+            src={URL.createObjectURL(selectedFile)}
+            alt={selectedFile.name}
+            className="h-full w-full object-cover"
+          />
+        </ReactCrop>
+      )}
+    </div>
+  );
+}
