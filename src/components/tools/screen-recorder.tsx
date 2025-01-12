024dd08e740fbda25feb52569f596cba57c5d988
Gaurav Kumar Yadav <gaurav@Gauravs-MacBook-Pro-2.local>
2025-01-12 21:45:55 +0530

    screen recorder changes

--- src/components/tools/screen-recorder.tsx
+++ src/components/tools/screen-recorder.tsx
@@ -1,22 +1,53 @@
 "use client";
 
-import { ButtonWithHandler } from "../lib/buttons";
-import { CustomSvgIcon } from "../lib/icons";
+import { ButtonWithHandler } from "@/components/lib/buttons";
+import { CustomSvgIcon } from "@/components/lib/icons";
 import MonitorIcon from "@/data/icons/monitor.svg";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import { useState } from "react";
-import { CheckBoxWithLabel } from "../lib/checkboxes";
 import PauseIcon from "@mui/icons-material/Pause";
 import { CircularProgress, Typography } from "@mui/material";
+import { VideoStreamMerger } from "video-stream-merger";
+import { CheckBoxWithLabel } from "@/components/lib/checkBoxes";
+import { clear, get, set } from "idb-keyval";
+import { SnackBarWithPosition } from "../lib/snackBar";
+import {
+  configureStreamRecorder,
+  getCameraAndMicrophoneStream,
+  getScreenStream,
+  mergeMediaStreams,
+} from "@/util/screenRecorderUtils";
 
 enum RecordingState {
   NOT_RECORDING,
   RECORDING,
   PAUSED,
-  PREPARING_RECORDING,
+  PROCESSING_RECORDING,
 }
 
 export default function ScreenRecorder() {
+  const cameraVideoOptions: Record<string, number> = {
+    width: 150,
+    height: 150,
+  };
+
+  const mediaRecoderOptionsConfig: Record<string, MediaRecorderOptions> = {
+    webm: {
+      mimeType: "video/webm; codecs=vp9",
+    },
+    mp4: {
+      mimeType: "video/mp4",
+    },
+  };
+
+  const RECORDING_START_DELAY_MS = 5000;
+  const RECORDER_TIME_SLICE_MS = 100;
+
+  let videoChunksIds: number[] = [];
+
+  const [isSupported, setIsSupported] = useState(true);
+  const [recordedVideoExtension, setRecordedVideoExtension] = useState("webm");
+
   const [recordingState, setRecordingState] = useState(
     RecordingState.NOT_RECORDING
   );
@@ -24,12 +55,134 @@ export default function ScreenRecorder() {
   const [includeSystemAudio, setIncludeSystemAudio] = useState(false);
   const [includeCameraVideo, setIncludeCameraVideo] = useState(false);
 
-  const startRecording = () => {
-    setRecordingState(RecordingState.RECORDING);
+  const [isSnackBarOpen, setIsSnackBarOpen] = useState(false);
+  const [snackBarMessage, setSnackBarMessage] = useState("");
+
+  const handleSnackBarClose = () => {
+    setIsSnackBarOpen(false);
   };
 
-  const stopRecording = () => {
-    setRecordingState(RecordingState.PREPARING_RECORDING);
+  const startRecording = async () => {
+    setRecordingState(RecordingState.RECORDING);
+
+    // reset videoChunksIds
+    videoChunksIds = [];
+
+    await clear();
+
+    let screenStream: MediaStream | undefined;
+    try {
+      screenStream = await getScreenStream({
+        includeSystemAudio,
+      });
+    } catch (error) {
+      setSnackBarMessage("Error starting recording");
+      setIsSnackBarOpen(true);
+      setRecordingState(RecordingState.NOT_RECORDING);
+    }
+
+    let webcamStream: MediaStream | undefined;
+    if (includeCameraVideo || includeMicrophoneAudio) {
+      try {
+        webcamStream = await getCameraAndMicrophoneStream({
+          includeCameraVideo,
+          includeMicrophoneAudio,
+        });
+      } catch (error) {
+        setSnackBarMessage("Error starting recording");
+        setIsSnackBarOpen(true);
+        setRecordingState(RecordingState.NOT_RECORDING);
+      }
+
+      if (screenStream) {
+        configureStreamStopListener(screenStream);
+      }
+
+      let mergedMediaStream: MediaStream | null;
+      if (webcamStream) {
+        configureStreamStopListener(webcamStream);
+
+        mergedMediaStream = mergeMediaStreams({
+          screenStream,
+          webcamStream,
+          includeSystemAudio,
+          includeMicrophoneAudio,
+          cameraVideoOptions,
+        });
+      } else {
+        mergedMediaStream = screenStream!;
+      }
+
+      const mediaRecorder = new MediaRecorder(
+        mergedMediaStream!,
+        mediaRecoderOptionsConfig[recordedVideoExtension]
+      );
+
+      if (screenStream && webcamStream && !mergedMediaStream) {
+        stopRecording({
+          screenStream,
+          webcamStream,
+          mergedMediaStream,
+          mediaRecorder,
+          setRecordingState,
+        });
+      }
+
+      if (mergedMediaStream) {
+        mediaRecorder.ondataavailable = (event: BlobEvent) => {
+          const id = Date.now();
+          /**
+           * write data in index db
+           */
+          set(id, event.data);
+          videoChunksIds.push(id);
+        };
+      }
+    }
+  };
+
+  const stopRecording = ({
+    screenStream,
+    webcamStream,
+    mergedMediaStream,
+    mediaRecorder,
+    setRecordingState,
+  }: Readonly<{
+    screenStream: MediaStream | undefined;
+    webcamStream: MediaStream | undefined;
+    mergedMediaStream: MediaStream | null;
+    mediaRecorder: MediaRecorder;
+    setRecordingState: React.Dispatch<React.SetStateAction<RecordingState>>;
+  }>) => {
+    mediaRecorder?.stop();
+    screenStream?.getTracks().forEach((track) => track.stop());
+    webcamStream?.getTracks().forEach((track) => track.stop());
+    mergedMediaStream?.getTracks().forEach((track) => track.stop());
+
+    setRecordingState(RecordingState.PROCESSING_RECORDING);
+  };
+
+  const processRecordingVideo = ({
+    setRecordingState,
+  }: Readonly<{
+    setRecordingState: React.Dispatch<React.SetStateAction<RecordingState>>;
+  }>) => {
+    setRecordingState(RecordingState.PROCESSING_RECORDING);
+  };
+
+  const configureStreamStopListener = (mediaStream: MediaStream) => {
+    mediaStream.getTracks().forEach((track) => {
+      track.addEventListener("ended", (event) => {
+        console.log(`media stream track has ended`);
+
+        /**
+         * If recording is in progress, stop recording
+         */
+        if (recordingState === RecordingState.RECORDING) {
+          setRecordingState(RecordingState.NOT_RECORDING);
+        }
+      });
+    });
   };
 
   const PreparingRecording = () => {
@@ -45,6 +198,12 @@ export default function ScreenRecorder() {
 
   return (
     <div className="flex flex-col items-center w-full gap-3">
+      <SnackBarWithPosition
+        message={snackBarMessage}
+        open={isSnackBarOpen}
+        autoHideDuration={2000}
+        handleClose={handleSnackBarClose}
+      />
       <CustomSvgIcon sx={{ fontSize: "10rem" }}>
         <MonitorIcon />
       </CustomSvgIcon>
@@ -84,7 +243,7 @@ export default function ScreenRecorder() {
           color="error"
         />
       )}
-      {recordingState === RecordingState.PREPARING_RECORDING && (
+      {recordingState === RecordingState.PROCESSING_RECORDING && (
         <PreparingRecording />
       )}
     </div>
---
+++ src/util/screenRecorderUtils.ts
@@ -0,0 +1,97 @@
+import { VideoStreamMerger } from "video-stream-merger";
+
+export async function getScreenStream({
+  includeSystemAudio = false,
+}: Readonly<{
+  includeSystemAudio?: boolean;
+}>) {
+  return navigator.mediaDevices.getDisplayMedia({
+    video: true,
+    audio: includeSystemAudio,
+  });
+}
+
+export function getVideoStreamHeightWidth(mediaStream: MediaStream) {
+  if (mediaStream.getVideoTracks().length > 0) {
+    const settings: MediaTrackSettings = mediaStream
+      .getVideoTracks()[0]
+      .getSettings();
+    return `[ height: ${settings.height}, width: ${settings.width} ]`;
+  }
+  return "";
+}
+
+export async function getCameraAndMicrophoneStream({
+  includeCameraVideo,
+  includeMicrophoneAudio,
+}: Readonly<{
+  includeCameraVideo: boolean;
+  includeMicrophoneAudio: boolean;
+}>) {
+  return navigator.mediaDevices.getUserMedia({
+    video: includeCameraVideo,
+    audio: includeMicrophoneAudio,
+  });
+}
+
+export function configureMediaStreamRecorder(
+  mediaStream: MediaStream,
+  mediaRecorderOptions: MediaRecorderOptions
+) {
+  return new MediaRecorder(mediaStream, mediaRecorderOptions);
+}
+
+export function mergeMediaStreams({
+  screenStream,
+  webcamStream,
+  includeSystemAudio,
+  includeMicrophoneAudio,
+  cameraVideoOptions,
+}: Readonly<{
+  screenStream: MediaStream | undefined;
+  webcamStream: MediaStream | undefined;
+  includeSystemAudio: boolean;
+  includeMicrophoneAudio: boolean;
+  cameraVideoOptions: Record<string, number>;
+}>): MediaStream | null {
+  const screenStreamSettings: MediaTrackSettings = screenStream
+    ?.getVideoTracks()[0]
+    .getSettings()!;
+
+  const mergerOptions: any = {
+    width: screenStreamSettings.width!,
+    height: screenStreamSettings.height!,
+  };
+
+  let streamMerger: VideoStreamMerger = new VideoStreamMerger(mergerOptions);
+
+  const mergeScreenStreamOptions: any = {
+    x: 0,
+    y: 0,
+    width: streamMerger.width,
+    height: streamMerger.height,
+    mute: !includeSystemAudio,
+    index: 0,
+  };
+
+  // Add the screen capture. Position it to fill the whole stream (the default)
+  streamMerger.addStream(screenStream!, mergeScreenStreamOptions);
+
+  const mergeWebcamStreamOptions: any = {
+    x: streamMerger.width - cameraVideoOptions.width,
+    y: streamMerger.height - cameraVideoOptions.height,
+    width: cameraVideoOptions.width,
+    height: cameraVideoOptions.height,
+    mute: !includeMicrophoneAudio,
+    index: 1,
+  };
+
+  // Add the webcam stream. Position it on the bottom left and resize it to 100x100.
+  streamMerger.addStream(webcamStream!, mergeWebcamStreamOptions);
+
+  // Start the merging. Calling this makes the result available to us
+  streamMerger.start();
+
+  // We now have a merged MediaStream!
+  return streamMerger.result;
+}
