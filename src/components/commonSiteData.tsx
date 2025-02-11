ecd6dc777f0cdbe909edfa7564739c318588122b
Gaurav Kumar Yadav <gaurav10610@gmail.com>
2025-02-11 16:52:29 +0530

    added disclaimer

--- src/components/commonSiteData.tsx
+++ src/components/commonSiteData.tsx
@@ -45,6 +45,46 @@ function CopyRight() {
   );
 }
 
+const Disclaimer = () => {
+  return (
+    <div className="w-full flex flex-col gap-1 mb-3">
+      <Typography
+        variant="body1"
+        color="warning"
+        className="!font-semibold !text-center"
+      >
+        Disclaimer
+      </Typography>
+      <Typography variant="caption" color="textSecondary">
+        The web tools provided on this website are offered for free and for
+        general informational or utility purposes only. We make no warranties
+        about the completeness, reliability, accuracy, or suitability of these
+        tools for any particular purpose. Use of these tools is at your sole
+        risk.
+      </Typography>
+      <Typography variant="caption" color="textSecondary">
+        <strong>No Data Storage or Transmission:</strong> We do not store,
+        collect, or transmit any user data entered into these tools outside of
+        your web browser. All processing and calculations occur locally within
+        your browser environment.
+      </Typography>
+      <Typography variant="caption" color="textSecondary">
+        <strong>External Links:</strong> This website may contain links to
+        external websites. We are not responsible for the content or privacy
+        practices of these websites.
+      </Typography>
+      <Typography variant="caption" color="textSecondary">
+        <strong>
+          By using this website and its tools, you agree to this disclaimer.
+        </strong>
+        We reserve the right to modify this disclaimer at any time without
+        notice. It is your responsibility to review this disclaimer periodically
+        for changes.
+      </Typography>
+    </div>
+  );
+};
+
 export async function CommonSiteData({
   className = "",
 }: {
@@ -54,6 +94,7 @@ export async function CommonSiteData({
     <div className={`flex flex-col items-center gap-3 ${className}`}>
       <SocialFollowButtons />
       <CopyRight />
+      <Disclaimer />
     </div>
   );
 }
