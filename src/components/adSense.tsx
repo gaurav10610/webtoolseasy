61e55367caa77f0e9fc25c632433a067599b54fb
gaurav kumar yadav <gaurav10610@gmail.com>
2025-08-02 21:36:22 +0530

    added adsense id

---
+++ src/components/adSense.tsx
@@ -0,0 +1,12 @@
+import Script from "next/script";
+
+export const AdSense = ({ pId }: { pId: string }) => {
+  return (
+    <Script
+      async
+      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${pId}`}
+      crossOrigin="anonymous"
+      strategy="afterInteractive"
+    />
+  );
+};
